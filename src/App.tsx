76d16a320334b743e1c58a866312970291e45402
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-10-16 13:00:27 -0300

    Some changes at the bubble sort function

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
         { name: 'Lionel Messi', goals: 896 },
         { name: 'Pelé', goals: 767 },
         { name: 'Romário', goals: 772 },
-        { name: 'Ferenc Puskás', goals: 746 },
         { name: 'Robert Lewandowski', goals: 598 },
         { name: 'Luis Suárez', goals: 520 },
         { name: 'Zlatan Ibrahimović', goals: 573 },
@@ -22,7 +21,6 @@ function App() {
         { name: 'Edinson Cavani', goals: 428 },
         { name: 'Raúl', goals: 404 },
         { name: 'David Villa', goals: 376 },
-        { name: 'Mario Kempes', goals: 377 },
         { name: 'Ruud van Nistelrooy', goals: 347 },
         { name: 'Andriy Shevchenko', goals: 372 },
         { name: 'Samuel Eto\'o', goals: 426 },
@@ -51,24 +49,19 @@ function App() {
         { name: 'José Luis Chilavert', goals: 67 },
         { name: 'Hans-Jörg Butt', goals: 32 },
         { name: 'René Higuita', goals: 41 },
-        { name: 'Jorge Campos', goals: 46 },
         { name: 'Mohamed Salah', goals: 270 },
         { name: 'Sadio Mané', goals: 211 },
         { name: 'Harry Kane', goals: 321 },
         { name: 'Antoine Griezmann', goals: 234 },
         { name: 'Pierre-Emerick Aubameyang', goals: 272 },
         { name: 'Romelu Lukaku', goals: 368 },
-        { name: 'Robert Pires', goals: 129 },
-        { name: 'Mesut Özil', goals: 108 },
         { name: 'Eden Hazard', goals: 185 },
         { name: 'Angel Di Maria', goals: 126 },
-        { name: 'Ivan Rakitić', goals: 94 },
         { name: 'Philippe Coutinho', goals: 121 },
         { name: 'Toni Kroos', goals: 100 },
         { name: 'Casemiro', goals: 46 },
         { name: 'Sergio Busquets', goals: 18 },
         { name: 'N’Golo Kanté', goals: 24 },
-        { name: 'Jorginho', goals: 34 },
         { name: 'Thomas Müller', goals: 277 },
         { name: 'Miroslav Klose', goals: 277 },
         { name: 'Marco Reus', goals: 159 },
@@ -76,8 +69,8 @@ function App() {
         { name: 'João Félix', goals: 80 },
         { name: 'Paulo Dybala', goals: 152 },
         { name: 'Roberto Firmino', goals: 164 },
-        { name: 'Ederson Moraes', goals: 1 },
         { name: 'Alisson Becker', goals: 2 },
+        { name: 'Ederson Moraes', goals: 1 },
         { name: 'Manuel Neuer', goals: 1 },
         { name: 'Paolo Maldini', goals: 40 },
         { name: 'Alessandro Nesta', goals: 20 },
@@ -85,7 +78,6 @@ function App() {
         { name: 'Marquinhos', goals: 38 },
         { name: 'Leonardo Bonucci', goals: 42 },
         { name: 'Virgil van Dijk', goals: 50 },
-        { name: 'Kyle Walker', goals: 12 },
         { name: 'Raphaël Varane', goals: 29 },
         { name: 'Kalidou Koulibaly', goals: 23 },
         { name: 'Aymeric Laporte', goals: 25 },
@@ -99,7 +91,82 @@ function App() {
         { name: 'Antonio Rüdiger', goals: 19 },
         { name: 'Trent Alexander-Arnold', goals: 15 },
         { name: 'Luke Shaw', goals: 8 },
-        { name: 'Giorgio Chiellini', goals: 37 }
+        { name: 'Giorgio Chiellini', goals: 37 },
+        { name: 'Bruno Fernandes', goals: 120 },
+        { name: 'Jadon Sancho', goals: 65 },
+        { name: 'Marcus Rashford', goals: 120 },
+        { name: 'Bukayo Saka', goals: 40 },
+        { name: 'Raheem Sterling', goals: 140 },
+        { name: 'Jack Grealish', goals: 40 },
+        { name: 'Phil Foden', goals: 60 },
+        { name: 'Mason Mount', goals: 55 },
+        { name: 'Jude Bellingham', goals: 30 },
+        { name: 'Kai Havertz', goals: 75 },
+        { name: 'Tammy Abraham', goals: 90 },
+        { name: 'Victor Osimhen', goals: 110 },
+        { name: 'Wilfried Zaha', goals: 70 },
+        { name: 'Jamie Vardy', goals: 190 },
+        { name: 'James Maddison', goals: 60 },
+        { name: 'Youri Tielemans', goals: 40 },
+        { name: 'Harvey Barnes', goals: 40 },
+        { name: 'James Ward-Prowse', goals: 55 },
+        { name: 'Ollie Watkins', goals: 65 },
+        { name: 'Ivan Toney', goals: 85 },
+        { name: 'Alexander Isak', goals: 75 },
+        { name: 'Martin Ødegaard', goals: 50 },
+        { name: 'Emile Smith Rowe', goals: 30 },
+        { name: 'Gabriel Jesus', goals: 120 },
+        { name: 'Richarlison', goals: 85 },
+        { name: 'Dominic Calvert-Lewin', goals: 70 },
+        { name: 'Raphinha', goals: 60 },
+        { name: 'Rodri', goals: 25 },
+        { name: 'Declan Rice', goals: 15 },
+        { name: 'Kalvin Phillips', goals: 10 },
+        { name: 'Fabinho', goals: 30 },
+        { name: 'Thiago Alcântara', goals: 25 },
+        { name: 'Georginio Wijnaldum', goals: 70 },
+        { name: 'Jorginho', goals: 34 },
+        { name: 'Yves Bissouma', goals: 10 },
+        { name: 'Enzo Fernández', goals: 15 },
+        { name: 'Casemiro', goals: 46 },
+        { name: 'Lucas Paquetá', goals: 55 },
+        { name: 'Rodrygo', goals: 45 },
+        { name: 'Vinícius Júnior', goals: 85 },
+        { name: 'Federico Valverde', goals: 35 },
+        { name: 'Eduardo Camavinga', goals: 15 },
+        { name: 'Aurelien Tchouameni', goals: 20 },
+        { name: 'Pedri', goals: 25 },
+        { name: 'Gavi', goals: 15 },
+        { name: 'Ansu Fati', goals: 35 },
+        { name: 'Ferran Torres', goals: 45 },
+        { name: 'Ousmane Dembélé', goals: 60 },
+        { name: 'Memphis Depay', goals: 145 },
+        { name: 'Gonzalo Higuaín', goals: 336 },
+        { name: 'Karim Adeyemi', goals: 50 },
+        { name: 'Christopher Nkunku', goals: 85 },
+        { name: 'Randal Kolo Muani', goals: 50 },
+        { name: 'Dusan Vlahovic', goals: 105 },
+        { name: 'Federico Chiesa', goals: 60 },
+        { name: 'Lorenzo Insigne', goals: 135 },
+        { name: 'Ciro Immobile', goals: 240 },
+        { name: 'Andrea Belotti', goals: 120 },
+        { name: 'Sandro Tonali', goals: 25 },
+        { name: 'Nicolo Barella', goals: 35 },
+        { name: 'Adrien Rabiot', goals: 40 },
+        { name: 'Manuel Locatelli', goals: 25 },
+        { name: 'Sergej Milinkovic-Savic', goals: 80 },
+        { name: 'Rafael Leão', goals: 85 },
+        { name: 'Domenico Berardi', goals: 125 },
+        { name: 'Paulo Dybala', goals: 152 },
+        { name: 'Giorgio Chiellini', goals: 37 },
+        { name: 'Leonardo Bonucci', goals: 42 },
+        { name: 'Gianluigi Donnarumma', goals: 1 },
+        { name: 'Kasper Schmeichel', goals: 1 },
+        { name: 'Samir Handanovic', goals: 1 },
+        { name: 'Pepe Reina', goals: 1 },
+        { name: 'Manuel Neuer', goals: 1 },
+        { name: 'Alisson Becker', goals: 2 },
+        { name: 'Ederson Moraes', goals: 1 }
     ];
 
     const [pickedPlayersArr, setPickedPlayersArr] = useState<Player[]>([]);
@@ -120,20 +187,21 @@ function App() {
             });
             setResult(acc);
         }
-        bubbleSort();
-    }, [pickedPlayersArr]);
 
-    function bubbleSort() {
-        for (let i = 0; i < pickedPlayersArr.length; i++) {
-            for (let j = 0; j < pickedPlayersArr.length; j++) {
-                if (pickedPlayersArr[j].goals > pickedPlayersArr[i].goals) {
-                    let aux = pickedPlayersArr[j];
-                    pickedPlayersArr[j] = pickedPlayersArr[i];
-                    pickedPlayersArr[i] = aux;
+        setPickedPlayersArr((prev) => {
+            const auxArr = [...prev]
+            for (let i = 0; i < auxArr.length; i++) {
+                for (let j = 0; j < auxArr.length; j++) {
+                    if (auxArr[j].goals > auxArr[i].goals) {
+                        let aux = auxArr[j];
+                        auxArr[j] = auxArr[i];
+                        auxArr[i] = { ...aux };
+                    }
                 }
             }
-        }
-    }
+            return auxArr;
+        });
+    }, [X1, X2, X3, X4]);
 
     return (
         <main>
@@ -232,8 +300,13 @@ function App() {
                     return <p>{`${player.name} - ${player.goals}`}</p>
                 })}
             </div>
-            <p>{result}</p>
-            <p>{result >= 9000 ? 'You Win!' : 'You Lose!'}</p>
+            {pickedPlayersArr.length >= 9 &&
+                <div className="modal_backdrop">
+                    <div className="modal_content">
+                        <p>{result}</p>
+                        <p>{result >= 9000 ? 'You Win!' : 'You Lose!'}</p>
+                    </div>
+                </div>}
         </main>
     )
 }
