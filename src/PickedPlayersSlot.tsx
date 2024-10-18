4df1ae744c8461fd1a92a90d4b2a86de424438d9
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-10-18 12:31:34 -0300

    Picked players now show their multipliers (x1, x2, x3, x4 and so on)

--- src/PickedPlayersSlot.tsx
+++ src/PickedPlayersSlot.tsx
@@ -8,8 +8,22 @@ type PickedPlayerProps = {
 function PickedPlayersSlot({ pickedPlayers, allPlayers }: PickedPlayerProps) {
     return (
         <div className="player_slot">
-            {pickedPlayers.map(player => {
-                return <p>{`${player.name} - ${player.goals}`}</p>
+            {pickedPlayers.map(pickedPlayer => {
+                let playerString = null;
+                allPlayers.forEach(player => {
+                    if (pickedPlayer.id === player.id) {
+                        if (pickedPlayer.goals === player.goals) {
+                            playerString = <p>{`X1: ${player.name} - ${player.goals}`}</p>;
+                        } else if (player.goals * 2 === pickedPlayer.goals) {
+                            playerString = <p>{`X2: ${player.name} - ${player.goals}`}</p>;
+                        } else if (player.goals * 3 === pickedPlayer.goals) {
+                            playerString = <p>{`X3: ${player.name} - ${player.goals}`}</p>;
+                        } else if (player.goals * 4 === pickedPlayer.goals) {
+                            playerString = <p>{`X4: ${player.name} - ${player.goals}`}</p>;
+                        }
+                    }
+                });
+                return playerString;
             })}
         </div>
     )
